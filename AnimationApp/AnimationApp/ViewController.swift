e0a388ba721834cf64c22099bbe040853cf1043e
Diante Lewis-Jolley <diantelewis-jolley@diantes-macbook-pro.local>
2019-05-22 17:08:23 -0400

    complete

--- AnimationApp/AnimationApp/ViewController.swift
+++ AnimationApp/AnimationApp/ViewController.swift
@@ -9,15 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var isScattered: Bool = false
+    var isScattered: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        lambdaArray += [lLabel, aLabel, mLabel, bLabel, bLabel, dLabel, lastaLabel]
+        imageView.image = UIImage(named: "lambda_logo")
+
     }
 
 
     @IBAction func toggleButtonPressed(_ sender: Any) {
+        setupAnimation()
+    }
+
+
+
+    private func setupAnimation() {
+        UIView.animate(withDuration: 3, animations: {
+            if self.isScattered {
+                self.imageView.alpha = 0
+                for letters in self.lambdaArray {
+                    let randomY = CGFloat.random(in: -15...15)
+                    let randomX = CGFloat.random(in: -20...10)
+                    letters.transform = CGAffineTransform(scaleX: randomX, y: -randomY)
+                    letters.transform = CGAffineTransform(rotationAngle: randomY)
+
+
+                }
+
+                self.isScattered = false
+            } else {
+                for letters in self.lambdaArray {
+                    letters.transform = .identity
+                }
+                self.imageView.alpha = 1.0
+                self.isScattered = true
+            }
+        })
     }
 
 
@@ -28,6 +59,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var dLabel: UILabel!
     @IBOutlet weak var lastaLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    var lambdaArray: [UILabel] = []
+
     
 
 }

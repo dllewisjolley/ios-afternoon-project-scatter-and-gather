0ceec3627238a90cfe0564d541f999abe0ea876b
Diante Lewis-Jolley <diantelewis-jolley@diantes-macbook-pro.local>
2019-05-22 15:57:49 -0400

    first commit

---
+++ AnimationApp/AnimationApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Diante Lewis-Jolley on 5/22/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var isScattered: Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+
+
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var lastaLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+}
+
